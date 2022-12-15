b3edce24dc5237228649d3e15cee24323c041299
Stirling-Silver <113132128+Stirling-Silver@users.noreply.github.com>
2022-12-15 11:22:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Script.js
+// HTML Elements
+
+let calcBtn = document.getElementById("calc-btn");
+let sumOut = document.getElementById("sum-out");
+
+//addEventListener
+calcBtn.addEventListener("click", calcSum);
+
+//Event Function
+function calcSum() {
+  //Sumintegers 1 to 100
+  //   let total = 0;
+  //   for (let n = 1; n <= 1000; n++) {
+  //     total = total + n;
+  //   }
+  let total = (100 * 101) / 2;
+
+  sumOut.innerHTML = total;
+}
